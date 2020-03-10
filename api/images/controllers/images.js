cf441e8f9339c9468e909aced9e5ad62ece7c81f
Saksham Khurana <dawnimpulse@gmail.com>
2020-03-10 23:11:47 +0530

    feat(new) : strapi random images ctrl

--- api/images/controllers/images.js
+++ api/images/controllers/images.js
@@ -1,8 +1,33 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+const {sanitizeEntity, convertRestQueryParams} = require("strapi-utils");
+
+module.exports = {
+
+  /**
+   * get random images
+   *
+   * @param ctx
+   * @field limit
+   * @return {Promise<void>}
+   */
+  random: async function (ctx) {
+    const query = convertRestQueryParams(ctx.request.query);
+    try {
+
+      const images = await strapi.query("images").model.aggregate(
+        [{$sample: {size: query.limit}}]
+      );
+
+      return images.map(image => sanitizeEntity(image, {model: strapi.models.images}));
+
+    } catch (e) {
+      return ctx.badImplementation(e);
+    }
+  }
+};
